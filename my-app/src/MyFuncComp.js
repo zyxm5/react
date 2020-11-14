55c13ddfbe53b653ec261b3fed4efc05669ad874
zhiyang <zhiyang3370@dingtalk.com>
2020-11-14 12:15:19 +0800

    m

--- my-app/src/MyFuncComp.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function MyFuncComp(props){
-    return <span>函数组件 : {props.name}</span>;
-}
--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MyFuncComp from "./MyFuncComp";
-import MyClassComp from './MyClassComp';
-
+import StudentList from "./components/StudentList";
 // 函数组件：最终返回一个react元素
 	// 关于属性，作为第一个参数传递
 // 类组件：继承React.Component, 实现render方法，并返回一个react元素
 //  关于属性，作为构造函数的参数进行传递
 
 const container = document.getElementById('root');
+const stus = new Array(100);
+stus.fill({
+	name: 'zt',
+	age: 25,
+	sex: 1
+})
 ReactDOM.render(<div>
-	{/* {MyFuncComp()} */}
-	<MyFuncComp name="zy"/>
-	<MyClassComp age={10} fun={() => (<h1>这是我使用函数传递过去的ui</h1>)}/>
-	<MyClassComp age={10} ui={<h2>这是我使用对象传递过去的ui</h2>}/>
-	<MyClassComp age={10}/>
+	<StudentList stus={stus}/>
 </div>, container);
