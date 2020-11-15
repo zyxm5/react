150caf2f3c7449b790d2fcff422efc48245989bb
zhiyang <zhiyang3370@dingtalk.com>
2020-11-15 17:31:46 +0800

    m

--- my-app/src/components/Test.js
+++ my-app/src/components/Test.js
@@ -1,27 +1,15 @@
 import React, { Component } from 'react'
-import Modal from './common/Modal';
+import ThreeLayout from './common/ThreeLayout';
 
 export default class Test extends Component {
-    state = {
-        showModal: true
-    }
-    closeModal = () => {
-        this.setState({
-            showModal: false
-        })
-    }
     render() {
         return (
             <div>
-                {
-                    this.state.showModal ? <Modal onClick={this.closeModal} bgColor={'rgba(255,0,0,.3)'}>
-                        <div style={{backgroundColor:'#fff', width: 500, height: 200}}>
-                            asdfasfasdfasf
-                            <button onClick={ this.closeModal }>关闭</button>
-                        </div>
-                    </Modal> : null
-                }
-                
+                <ThreeLayout left={'左侧区域内容'} right={'右侧区域内容'}>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit officia harum eveniet doloribus temporibus, eaque, nisi ex debitis quisquam assumenda repudiandae architecto. Magnam sunt, distinctio consectetur cum exercitationem asperiores esse?
+                    </p>
+                </ThreeLayout>
             </div>
         )
     }
